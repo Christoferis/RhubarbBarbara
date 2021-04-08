4ee76ac268602a0994a8a4ec88373b5714c67cc3
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-04-08 19:05:08 +0200

    GUI in Progress

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #RhubarbBarbara is a Python tool for using Rhubarb
 
-from tkinter.constants import LEFT, RIGHT
-import moviepy as mov
 import tkinter as tk
-from PIL import Image
+import tkinter.filedialog as fd
+from tkinter.constants import BOTH, BOTTOM, END, LEFT, RIGHT, TOP, X, Y
 
+import moviepy as mov
+from PIL import Image, ImageTk
 
-
+mouthshapes = dict()
 
 #ⒶⒷⒸⒹⒺⒻ extended shapes(ⒼⒽⓍ)
 
@@ -16,13 +17,33 @@ class mouthSelector:
 
     def __init__(self, window, mouthshape):
         self.window = window
-        self.logo = mouthshape
+        self.logo = mouthshape + "\n "
+        
 
         #mouthshape selector:
-        if mouthshape in ("Ⓐ","Ⓑ","Ⓒ","Ⓓ","Ⓔ","Ⓕ"):
+        if mouthshape in ("A","B","C","D","E","F"):
             self.ext = False
         else:
             self.ext = True
+
+        if mouthshape == "A":
+            self.image = "icons\lisa-A.png"
+        elif mouthshape == "B":
+            self.image = "icons\lisa-B.png"
+        elif mouthshape == "C":
+            self.image = "icons\lisa-C.png"
+        elif mouthshape == "D":
+            self.image = "icons\lisa-D.png"
+        elif mouthshape == "E":
+            self.image = "icons\lisa-E.png"
+        elif mouthshape == "F":
+            self.image = "icons\lisa-F.png"
+        elif mouthshape == "G":
+            self.image = "icons\lisa-G.png"
+        elif mouthshape == "H":
+            self.image = "icons\lisa-H.png"
+        elif mouthshape == "X":
+            self.image = "icons\lisa-X.png"
         
         pass
 
@@ -31,34 +52,54 @@ class mouthSelector:
         shape = tk.Frame(self.window)
         
         #mouthshape îdentifier
-        tk.Label(shape, text=self.mouthshape).pack(side=LEFT)
-        #Image preview
+        if self.ext == True:
+            prevstr = self.logo + "\n " + "(Optional)"
+        else:
+            prevstr = self.logo
+
+        tk.Label(shape, text=prevstr).pack(side=LEFT)
+
+        #Mouthshape preview (https://github.com/DanielSWolf/rhubarb-lip-sync#readme)
+        img = ImageTk.PhotoImage(Image.open(self.image).resize((75, 62)))
+        imgframe = tk.Label(shape, image=img)
+        imgframe.image = img
+        imgframe.pack(side=LEFT)
+
+        #button to open filedialog
+        tk.Button(shape, text="add a image", command=self.add_to_dict).pack(side=RIGHT, fill=BOTH)
         
+        return shape
 
-
-        #build frame
-        shape.pack()
-        return 
-
+    def add_to_dict(self):
+        global mouthshapes
+        shape = fd.askopenfile()
+        
+        #mouthshapes[self.logo] = shape
 
 
 def gui(window):
     #make main Frame for the images
+    top = tk.Frame(window)
+
+    for shape in ("X","H","G","F","E","D","C","B","A"):
+        frame = mouthSelector(window=top, mouthshape=shape)
+        frame.create_wid().pack(side=BOTTOM)
+
+    top.pack(expand=2, pady=50, fill=BOTH)
 
     #Frame for the start Button and Audio Input
     bottom = tk.Frame(window)
     
-    tk.Text(bottom).pack(side=LEFT)
+    audiopath = tk.Entry(bottom)
+    audiopath.pack(side=LEFT, expand=1, fill=X)
+    audiopath.insert(END, "Path to audio")
 
     #start button
     tk.Button(bottom, text="start").pack(side=RIGHT)
     #search file button
     tk.Button(bottom, text="Open Audio File").pack(side=RIGHT)
 
-    bottom.pack()
-
-
-
+    bottom.pack(side=BOTTOM, fill=BOTH, expand=2)
 
 
 def main():
