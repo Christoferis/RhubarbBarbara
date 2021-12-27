bf9ff8e076778c0345203f75374db24ba7e47f88
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-12-27 14:07:16 +0100

    implementing mouth selector widget

--- bin/ui.py
+++ bin/ui.py
@@ -24,16 +24,39 @@ class m_selector(QtWidgets.QWidget):
     def __init__(self, parent, image) -> None:
         super().__init__(parent=parent)
 
-        self.layout = QtWidgets.QHBoxLayout()
+        self.image = None
+        self.layout = QtWidgets.QHBoxLayout(parent=self)
 
         #create the image
-        self.layout.addWidget(QtWidgets.QLabel().setPicture(QtGui.QPicture().load(image)))
+        self.preview = QtWidgets.QLabel(parent=self).setPicture(QtGui.QPicture().load(image))
 
         #buttons: x and select
-        self.layout.addWidget(QtWidgets.QPushButton)
+        load_button = QtWidgets.QPushButton(text="Load", parent=self)
+        x_button = QtWidgets.QPushButton(text="X", parent=self)
 
+        self.layout.addWidget(self.preview)
+        self.layout.addWidget(QtWidgets.QLabel(parent=self, text="A"))
+        self.layout.addWidget(load_button)
+        self.layout.addWidget(x_button)
+        
+        load_button.clicked.connect(self.set_image)
+
+
+    
+    @QtCore.Slot
     def set_image(self):
+        self.image = QtWidgets.QFileDialog.getOpenFileName()
+
+        #set image as Icon Backgorund
+        self.preview.setPicture(QtGui.QPicture().load(self.image))
+
         pass
+
+    def remove_image(self):
+        self.image = None
+
+    def get_image(self):
+        return self.image
 
         
 
